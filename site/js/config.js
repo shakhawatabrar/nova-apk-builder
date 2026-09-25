0ac292816fb2e797fd53ac8ee60f61dc0848860d
shakhawatabrar <shakhawat1858@gmail.com>
2026-09-26 03:53:48 +0600

    Update config.js

--- site/js/config.js
+++ site/js/config.js
@@ -6,6 +6,9 @@ const CONFIG = {
   GITHUB_OWNER: 'shakhawatabrar',
   GITHUB_REPO:  'nova-apk-builder',
   API_BASE: 'https://api.github.com',
+  
+  GITHUB_CLIENT_ID: 'Ov23li198RnSoehFRs4K',
+ 
   MAX_ZIP_MB: 50,
   POLL_INTERVAL_MS: 8000,
   BUILD_TIMEOUT_MS: 30 * 60 * 1000,  // 30 minutes
